92aedde2d146127287d54e342f59704b479319e9
Eric Munyi <irerieric@gmail.com>
2024-01-19 21:07:15 +0300

    models for posts likes and followers

--- network/models.py
+++ network/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Follower(models.Model):
+    follower = models.ForeignKey(
+        User, related_name='follower', on_delete=models.CASCADE)
+    following = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE)
