9b0268e6d89197db02fb693022f4ab7668e6489f
Eric Munyi <irerieric@gmail.com>
2024-02-19 20:06:24 +0300

    update

--- network/models.py
+++ network/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 class Post(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        AbstractUser, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return f"Post by {self.user.username} at {self.timestamp}"
