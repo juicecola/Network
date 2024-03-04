07e5eb13bdbc9cf279147dcf41a43c4f6b52234f
Eric Munyi <irerieric@gmail.com>
2024-03-04 10:30:32 +0300

    removed a bg

--- network/models.py
+++ network/models.py
@@ -1,12 +1,17 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    # Add any additional fields or methods here
+    pass
 
 
 class Post(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        AbstractUser, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return f"Post by {self.user.username} at {self.timestamp}"
